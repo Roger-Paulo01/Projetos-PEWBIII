5699f9d0dfc1f873395be823ae35dd0c77161566
Roger-Paulo01 <rel.cliente.r@gmail.com>
2026-04-09 22:29:48 -0300

    Montadora do Zero

--- Montadora/src/br/com/montadora/dao/CarroDAO.java
+++ Montadora/src/br/com/montadora/dao/CarroDAO.java
@@ -1,6 +1,10 @@
 package br.com.montadora.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.doceria.model.Beijinho;
 
 public class CarroDAO {
 	private Connection con = null;
@@ -11,5 +15,22 @@ public class CarroDAO {
 
 	public void setCon(Connection con) {
 		this.con = con;
+	}
+	
+	public String inserir(Beijinho beijinho) {
+		String sql = "insert into beijinho(nomedoce, valor, pesococo) values (?,?,?)";
+		try {
+			PreparedStatement ps = getCon().prepareStatement(sql);
+			ps.setString(1, beijinho.getNomeDoce());
+			ps.setDouble(2, beijinho.getValor());
+			ps.setDouble(3, beijinho.getPesoCoco());
+			if (ps.executeUpdate() > 0) {
+				return "Inserido com sucesso";
+			} else {
+				return "Erro inserir";
+			}
+		} catch (SQLException e) {
+			return e.getMessage();
+		}
 	}
 }
